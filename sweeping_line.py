53c281a83a147180421e7502d1b91ded2420decc
Benjamin Hackl <devel@benjamin-hackl.at>
2022-07-31 13:30:13 +0200

    added some comments

--- sweeping_line.py
+++ sweeping_line.py
@@ -2,6 +2,7 @@ from manim import *
 from colour import Color
 import random
 
+
 class SweepingLine(Scene):
     def construct(self):
         growing_circle = Circle(radius=0.001)
@@ -9,25 +10,28 @@ class SweepingLine(Scene):
         moving_line = Line([-7, -5, 0], [-6, 5, 0])
         moving_line.normal_vector = moving_line.copy().rotate(90*DEGREES).get_vector()
 
-        def opacity_updater(obj: VMobject):
-            # if sum((growing_circle.points[0] - growing_circle.get_center())**2) >= sum((obj.get_center() - growing_circle.get_center())**2):
-            if round(
-                get_winding_number(growing_circle.get_anchors() - obj.get_center())
-            ) > 0:
+        def opacity_updater(obj):
+            if (  # check whether dot is inside circle
+                sum((growing_circle.points[0] - growing_circle.get_center())**2)
+                >= sum((obj.get_center() - growing_circle.get_center())**2)
+                #  round(
+                #      get_winding_number(growing_circle.get_anchors() - obj.get_center())
+                #  ) > 0
+            ):
                 obj.set_fill(BLUE, opacity=1)
-                obj.clear_updaters()
-                obj.add_updater(color_updater)
+                obj.clear_updaters()  # removes opacity_updater, ...
+                obj.add_updater(color_updater)  # and attaches the color_updater function
                 self.add_sound("assets/click.wav")
 
         def color_updater(obj):
-            if (
-                np.dot(moving_line.get_start(), moving_line.normal_vector)
-                > np.dot(obj.get_center(), moving_line.normal_vector)
+            if (  # check whether point is *left* of the line
+                np.dot(obj.get_center(), moving_line.normal_vector)
+                < np.dot(moving_line.get_start(), moving_line.normal_vector)
             ):
                 if obj.color != Color(BLUE):
                     obj.set_color(BLUE)
                     self.add_sound("assets/click.wav")
-            else:
+            else:  # otherwise point is *right* of the line
                 if obj.color != Color(YELLOW):
                     obj.set_color(YELLOW)
                     self.add_sound("assets/click.wav")
