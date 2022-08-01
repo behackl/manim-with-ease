004f15e81d3c90668bf207b6c3e6d31f94fd9c2b
Benjamin Hackl <devel@benjamin-hackl.at>
2022-08-01 19:02:03 +0200

    fixed comments, added DeCasteljau scene

--- sweeping_line.py
+++ sweeping_line.py
@@ -14,7 +14,7 @@ class SweepingLine(Scene):
             if (  # check whether dot is inside circle
                 sum((growing_circle.points[0] - growing_circle.get_center())**2)
                 >= sum((obj.get_center() - growing_circle.get_center())**2)
-                #  round(
+                #  round(  # more general winding number approach!
                 #      get_winding_number(growing_circle.get_anchors() - obj.get_center())
                 #  ) > 0
             ):
@@ -24,14 +24,14 @@ class SweepingLine(Scene):
                 self.add_sound("assets/click.wav")
 
         def color_updater(obj):
-            if (  # check whether point is *left* of the line
+            if (  # check whether point is *right* of the line
                 np.dot(obj.get_center(), moving_line.normal_vector)
                 < np.dot(moving_line.get_start(), moving_line.normal_vector)
             ):
                 if obj.color != Color(BLUE):
                     obj.set_color(BLUE)
                     self.add_sound("assets/click.wav")
-            else:  # otherwise point is *right* of the line
+            else:  # otherwise point is *left* of the line
                 if obj.color != Color(YELLOW):
                     obj.set_color(YELLOW)
                     self.add_sound("assets/click.wav")
@@ -51,4 +51,72 @@ class SweepingLine(Scene):
         self.play(Create(moving_line))
         self.play(moving_line.animate.shift(14*RIGHT), run_time=5)
         self.play(moving_line.animate.shift(14*LEFT), run_time=5)
+
+
+
+
+class DeCasteljau(Scene):
+    def construct(self):
+        self.camera.background_color = '#455D3E'
+        a1 = np.array([-3, -2, 0])
+        h1 = np.array([-3, 0, 0])
+        h2 = np.array([3, 0, 0])
+        a2 = np.array([3, 2, 0])
+
+        t = ValueTracker(0.001)
+        self.add(t)
+
+        d01 = Cross(scale_factor=0.2).move_to(a1)
+        d02 = Dot(color=ORANGE).move_to(h1)
+        d03 = Dot(color=ORANGE).move_to(h2)
+        d04 = Cross(scale_factor=0.2).move_to(a2)
+
+        d11 = Dot(color=GRAY).add_updater(
+            lambda mob: mob.move_to(
+                (1- t.get_value()) * d01.get_center() + t.get_value() * d02.get_center()
+            )
+        )
+        d12 = Dot(color=GRAY).add_updater(
+            lambda mob: mob.move_to((1- t.get_value()) * d02.get_center() + t.get_value() * d03.get_center())
+        )
+        d13 = Dot(color=GRAY).add_updater(
+            lambda mob: mob.move_to((1- t.get_value()) * d03.get_center() + t.get_value() * d04.get_center())
+        )
+
+        d21 = Dot(color=GRAY).add_updater(
+            lambda mob: mob.move_to((1- t.get_value()) * d11.get_center() + t.get_value() * d12.get_center())
+        )
+        d22 = Dot(color=GRAY).add_updater(
+            lambda mob: mob.move_to((1- t.get_value()) * d12.get_center() + t.get_value() * d13.get_center())
+        )
+
+        d31 = Dot(color=RED).add_updater(
+            lambda mob: mob.move_to((1- t.get_value()) * d21.get_center() + t.get_value() * d22.get_center())
+        )
+
+        static_lines = [
+            Line(d01.get_center(), d02, color=ORANGE),
+            Line(d02, d03),
+            Line(d03, d04.get_center(), color=ORANGE)
+        ]
+
+        dynamic_lines = [
+            always_redraw(lambda a=a, b=b: Line(a.get_center(), b.get_center(), color=LIGHT_GRAY))
+            for a, b in [
+                (d11, d12), (d12, d13), (d21, d22)
+            ]
+        ]
+        self.add(*dynamic_lines, *static_lines, d01, d02, d03, d04, d11, d12, d13, d21, d22, d31)
+        self.add(
+            TracedPath(lambda: d31.get_center(), stroke_color=RED)
+        )
+
+        self.wait(0.5)
+        self.play(t.animate(run_time=5).set_value(0.999))
+        self.wait(0.5)
+        self.play(FadeOut(VGroup(
+            *dynamic_lines, *static_lines, d02, d03, d11, d12, d13, d21, d22
+        )))
+        self.wait()
+
 
